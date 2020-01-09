b6e7ef0757e127a9dffc8f16d7468a2e103de98b
RafaTrik <RafaTrik@gmail.com>
2020-01-08 21:57:51 -0500

    Vuelta a cambios previos v2

--- Qoper_Profiles/upload_image.php
+++
@@ -1,9 +0,0 @@
-<?php if ($handle = opendir('profielfotos/')) {
-    $file = //mysql_real_escape_string($_GET['u']); Aqui se pone la direccion pertinente de la BD de Qoper
-    if (file_exists('profielfotos/'.$file.'.png')) {
-        echo 'profielfotos/'.$file.'.png';
-    } else if (file_exists('profielfotos/'.$file.'.jpg')) {
-        echo 'profielfotos/'.$file.'.jpg';
-    } //Las cuatro lineas de arriba permiten que se utilizen archivos .jpg y .png, las mantiene para que no se pierdan
-}
-closedir($handle); 
