36fa813e4058fbc27500208e9e178f65afbce6f9
maitcsan23 <macuervo84@gmail.com>
2020-07-03 11:44:30 -0500

    conxion con BD en clever cloud, modulos de inicio

---
+++ FrontEnd/src/components/ladingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+
+export default class notesList extends Component {
+    render() {
+        return (
+            <p>PAGINA PRINCIPAL</p>
+        );
+    }
+}
