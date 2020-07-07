d64cc00859efe0e8821c3d53192c51874275872d
RafaTrik <RafaTrik@gmail.com>
2020-07-06 19:25:50 -0500

    Problemas de casing solucionados. Nueva navegación incompleta añadida.

--- FrontEnd/src/components/layout/navigation.js
+++ FrontEnd/src/components/layout/navigation.js
@@ -1,19 +1,40 @@
-import React  from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import AuthOptions from '../auth/AuthOptions'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/Appbar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import logoImg from '../../images/LOGO-TEXTO.png'
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    width: 135,
+    height: 43.54
+  },
+  appBarBackground: {
+    backgroundColor: '#1a237e'
+  }
+}));
 
 export default function Navigation() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container">
-                    
-                    <Link className="navbar-brand" to="/">
-                        Qoper
-                    </Link>
-                    
-                    <AuthOptions/>
-                  
-                </div>
-            </nav>
-        )
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="sticky" className={classes.appBarBackground}>
+        <Toolbar>
+          <Box p={1}>
+            <Link>
+              <Button>
+                <img src={logoImg} alt="Logo" width="130px" />
+              </Button>
+            </Link>
+          </Box>
+        </Toolbar>
+        <AuthOptions />
+      </AppBar>
+    </div>
+  );
 }
